83dc88217858a16233101d589a4d101f6273b642
thillo <salysem90@gmail.com>
2024-09-16 13:10:43 +0200

    managing products in app.js

--- src/app.js
+++ src/app.js
@@ -120,7 +120,171 @@ async function main() {
                 console.log("5. Retourner Au Menu principal");
                 console.log("6. Quitter");
                 const choix2 = readlineSync.question("Choisissez une option :");
-            
+                switch (choix2) {
+          case "1":
+            const products = await productModule.getProduct();
+            console.table(products);
+            main();
+            break;
+
+          case "2":
+            const name = readlineSync.question("Nom du produit: ");
+            const description = readlineSync.question(
+              "description du  produit: "
+            );
+            let price;
+            while (true) {
+              price = readlineSync.question("prix du produit: ");
+              if (!isNaN(price) && price.trim() !== "") {
+                price = parseFloat(price);
+                break;
+              }
+              console.log("Le prix doit etre un nombre.");
+            }
+
+            let stock;
+            while (true) {
+              stock = readlineSync.question("stock du produit: ");
+              if (
+                !isNaN(stock) &&
+                Number.isInteger(parseFloat(stock)) &&
+                stock.trim() !== ""
+              ) {
+                stock = parseInt(stock);
+                break;
+              }
+              console.log(
+                "Le stock doit etre un nombre."
+              );
+            }
+
+            console.log(`Prix: ${price}, Stock: ${stock}`);
+            const category = readlineSync.question("category du produit: ");
+            const barcode = readlineSync.question("barre-code du produit: ");
+            const status = readlineSync.question("status du produit: ");
+            const productId = await productModule.addProduct(
+              name,
+              description,
+              price,
+              stock,
+              category,
+              barcode,
+              status
+            );
+            console.log(`Produit ajouté avec l'id ${productId}`);
+            main();
+            break;
+
+          case "3":
+            const prouit = await productModule.getProduit();
+            let cmdExists = false;
+            let id;
+
+            while (!cmdExists) {
+              id = readlineSync.question(`Entrez l'id du produit : `);
+              id = parseInt(id);
+              for (let i = 0; i < prouit.length; i++) {
+                if (id === prouit[i]) {
+                  cmdExists = true;
+                  console.log("produit trouvé, id:", id);
+                  break;
+                }
+              }
+
+              if (!cmdExists) {
+                console.log("Ce produit n'existe pas, veuillez réessayer.");
+              }
+            }
+            const newName = readlineSync.question(
+              "Entrez le  nouveau nom du produit: "
+            );
+            const newDescription = readlineSync.question(
+              "Nouvelle description du produit: "
+            );
+            let newPrice;
+            while (true) {
+              newPrice = readlineSync.question("prix du produit: ");
+              if (!isNaN(newPrice) && newPrice.trim() !== "") {
+                newPrice = parseFloat(newPrice);
+                break;
+              }
+              console.log("Le prix doit etre un nombre.");
+            }
+
+            let newStock;
+            while (true) {
+              newStock = readlineSync.question("newStock du produit: ");
+              if (
+                !isNaN(newStock) &&
+                Number.isInteger(parseFloat(newStock)) &&
+                newStock.trim() !== ""
+              ) {
+                newStock = parseInt(newStock);
+                break;
+              }
+              console.log(
+                "Le stock doit etre un nombre."
+              );
+            }
+            const newcategory = readlineSync.question(
+              "Nouvelle catégorie du produit: "
+            );
+            const newBarecode = readlineSync.question(
+              "Nouveau code barre du produit: "
+            );
+            const newStatus = readlineSync.question(
+              "Nouveau statut du produit: "
+            );
+
+            await productModule.updateProduct(
+              id,
+              newName,
+              newDescription,
+              newPrice,
+              newStock,
+              newcategory,
+              newBarecode,
+              newStatus
+            );
+            console.log("produit modifié");
+            main();
+            break;
+          case "4":
+           
+            const proui = await productModule.getProduit();
+            let cExists = false;
+            let  deleteId;
+
+            while (!cExists) {
+              deleteId = readlineSync.question(`ID du produit à supprimer: `);
+              deleteId = parseInt(deleteId);
+              for (let i = 0; i < proui.length; i++) {
+                if (deleteId === proui[i]) {
+                  cExists = true;
+                  console.log("produit trouvé, Id:", deleteId);
+                  break;
+                }
+              }
+
+              if (!cExists) {
+                console.log("Ce produit n'existe pas, veuillez réessayer.");
+              }
+            }
+            await productModule.destroyProduct(deleteId);
+            console.log("Produit supprimé");
+            main();
+            break;
+          case "5":
+            main();
+            break;
+          case "6":
+            console.log("Au revoir !");
+            break;
+
+          default:
+            console.log("Veuillez choisir une option entre 1 et 6");
+            break;
+        }
             
                 break;
 
